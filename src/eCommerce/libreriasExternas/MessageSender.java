64a80ccd8318a696cda3b8c236fe560f5c6bd5a3
Renata <pastorinirenata@outlook.com>
2026-06-27 14:41:38 -0300

    Terminamos las notificaciones para los distintos estados del pedido

---
+++ src/eCommerce/libreriasExternas/MessageSender.java
@@ -0,0 +1,5 @@
+package eCommerce.libreriasExternas;
+
+public interface MessageSender {
+	public void enviarMensaje(Integer nroTelefono ,String mensaje);
+}
--- src/eCommerce/suscriptores/Fidelizador.java
+++ src/eCommerce/suscriptores/Fidelizador.java
@@ -1,15 +1,19 @@
 package eCommerce.suscriptores;
 
-import eCommerce.Pedido;
+import eCommerce.libreriasExternas.MessageSender;
 
 public class Fidelizador implements PedidoObserver {
-	public void notificarPedidoConfirmado(){}
-	
-	public void notificarPedidoEnviado(){}
-	
-	public void notificarPedidoEntregado(Pedido pedido){}
-	
-	public void notificarPedidoCancelado() {}
+	private Integer nroTelefono;
+	private MessageSender api;
+
+	public Fidelizador(Integer nroTelefono, MessageSender api) {
+		this.nroTelefono = nroTelefono;
+		this.api = api;
+	}
+
+	public void notificarPedidoCancelado(){
+		this.api.enviarMensaje(this.nroTelefono, "¡¡Volve y te regalamos el cupon 1234 para que obtengas un 5% de descuento aplicable en todos los productos de la tienda!!");
+	}
 
 
 }
